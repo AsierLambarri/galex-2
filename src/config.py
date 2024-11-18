90bdac75298c0ff5bacd8ec5f605f237c831ff26
BR <109964584+AsierLambarri@users.noreply.github.com>
2024-11-18 23:22:00 +0100

    Add user defined loader and parser in config, with default methods using yt

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import yt
+
 class Config:
     """Config class that provides configuration options for pkg. Here one can set the loader, units, conversion tables
     etc. to be used.
@@ -12,8 +14,28 @@ class Config:
         return cls._instance
 
     def _initialize_config(self):
-        """Initialize the default configuration settings."""
-        self.loader = None
+        """Initialize the default configuration settings. It initializes the default snap loader and data parser
+        (extract data with internal units, change to working units and save relevant simulation parameters such as
+        cosmology, boxsize, particle numbers etc.) that may be overrun by custom user functions if whished, 
+        base units of the simulation and the working units.
+        The base units are not always used, e.g. when data is loaded with defaults, i.e. using the yt package, units
+        are directly in place thanks to unyt.
+
+        All attributes may be overrun by the user.
+        
+        List of attributes
+        ------------------
+        loader : func
+            Loader of data
+        parser : func
+            Parser of data: extracts raw data, units, parameters, ...
+        base_units : dict[str : str]
+            Units of the simulation.
+        working_units : dict[str : str]
+            Units to work with.
+        """
+        self.loader = default_loader
+        self.parser = default_parser
         self.base_units = None
         
         self.working_units =  {
@@ -24,7 +46,15 @@ class Config:
             'comoving': False
         }
 
+    def default_loader(fn):
+        """Default loader. Returns yt.dataset
+        """
+        return yt.load(fn)
 
+    def default_parser(dataset):
+        """Default parser: extracts data from selected region with working units, and gets relevant metadata
+        """
+        return None
 
 
 config = Config()
