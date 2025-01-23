e95ae84c1b5991604ac2ba835f22ed04da50f0c4
alambarri <asierlambarri@gmail.com>
2025-01-23 13:35:54 +0100

    pt

--- galexquared/tracking.py
+++ galexquared/tracking.py
@@ -2,7 +2,8 @@ import yt
 import h5py
 import numpy as np
 import pandas as pd
-from pytreegrav import Potential, PotentialTarget
+from pathlib import Path
+import warnings
 
 from .config import config
 from .class_methods import load_ftable
@@ -109,6 +110,7 @@ class Tracker:
                 dtype=np.float32,
                 fillvalue=np.nan
             )
+            
     def _create_snapztdir(self):
         """Creates _snap_z_time and adds column with relevant particle file directories.
         """
@@ -130,6 +132,8 @@ class Tracker:
             self._thtrees[f"sub_tree_{int(subtree)}"] = pd.DataFrame(columns=columns, index=range(cols))
             self._thtrees[f"sub_tree_{int(subtree)}"]["Snapshot"] = list(range(int(sn), int(self.max_snap + 1)))
             self._thtrees[f"sub_tree_{int(subtree)}"]["Sub_tree_id"] = int(subtree)
+        
+        
         
     def set_catalogue(self, complete_tree):
         """Sets trees by loading the complete tree and separating the principal-leaf, main and satellite-trees. 
@@ -184,52 +188,198 @@ class Tracker:
         """
         return None
     
+    def compute_particle_gravity(self, **kwargs):
+        """Adds gravitational potential for stars.
+        """
+        def _grav_stars(field, data):
+            from pytreegrav import PotentialTarget    
+
+            return data["stars", "mass"] * unyt_array(
+                PotentialTarget(
+                    pos_source=np.concatenate( (data["stars", "coordinates"], data["darkmatter", "coordinates"]) ).to("kpc"), 
+                    pos_target=data["stars", "coordinates"].to("kpc"), 
+                    m_source=np.concatenate( (data["stars", "mass"], data["darkmatter", "mass"]) ).to("Msun"), 
+                    softening_target=data["stars", "softening"].to("kpc"),
+                    softening_source=np.concatenate( (data["stars", "softening"], data["darkmatter", "softening"]) ).to("kpc"), 
+                    G=4.300917270038e-06,
+                    theta=0.6,
+                    parallel=True,
+                    quadrupole=True
+                ), 
+                "km**2/s**2"
+            ) 
+        def _grav_dm(field, data):
+            from pytreegrav import PotentialTarget    
+
+            return data["darkmatter", "mass"] * unyt_array(
+                PotentialTarget(
+                    pos_source=np.concatenate( (data["stars", "coordinates"], data["darkmatter", "coordinates"]) ).to("kpc"), 
+                    pos_target=data["darkmatter", "coordinates"].to("kpc"), 
+                    m_source=np.concatenate( (data["stars", "mass"], data["darkmatter", "mass"]) ).to("Msun"), 
+                    softening_target=data["darkmatter", "softening"].to("kpc"),
+                    softening_source=np.concatenate( (data["stars", "softening"], data["darkmatter", "softening"]) ).to("kpc"), 
+                    G=4.300917270038e-06,
+                    theta=0.6,
+                    parallel=True,
+                    quadrupole=True
+                ), 
+                "km**2/s**2"
+            )
+        
+        self.ds.add_field(
+            ("stars", "grav_potential"),
+            function=_grav_stars,
+            sampling_type="local",
+            units="Msun*km**2/s**2",
+            force_override=True
+        )
+        self.ds.add_field(
+            ("stars", "grav_potential"),
+            function=_grav_dm,
+            sampling_type="local",
+            units="Msun*km**2/s**2",
+            force_override=True
+        )
     
+    def compute_particle_kinetic(self, v_cm, **kwargs):
+        """Computes the kinetic energy of the particles with respect to the center of mass.
+        """
+        self.ds.add_field(
+            ("stars", "kinetic_energy"),
+            function=lambda field, data: 0.5 * data["stars", "mass"] * np.linalg.norm(data["stars","velocity"] - v_cm, axis=1) ** 2,
+            sampling_type="local",
+            units="Msun*km**2/s**2",
+            force_override=True
+        )
+        self.ds.add_field(
+            ("darkmatter", "kinetic_energy"),
+            function=lambda field, data: 0.5 * data["darkmatter", "mass"] * np.linalg.norm(data["darkmatter","velocity"] - v_cm, axis=1) ** 2,
+            sampling_type="local",
+            units="Msun*km**2/s**2",
+            force_override=True
+        )
+
+    def compute_particle_energy(self, **kwargs):
+        """Computes the kinetic energy of the particles with respect to the center of mass.
+        """
+        self.ds.add_field(
+            ("stars", "total_energy"),
+            function=lambda field, data: data["stars", "kinetic_energy"] + data["stars", "grav_potential"],
+            sampling_type="local",
+            units="Msun*km**2/s**2",
+            force_override=True
+        )
+        self.ds.add_field(
+            ("darkmatter", "total_energy"),
+            function=lambda field, data: data["darkmatter", "kinetic_energy"] + data["darkmatter", "grav_potential"],
+            sampling_type="local",
+            units="Msun*km**2/s**2",
+            force_override=True
+        )
+    
+    def select_particles_init(self, center, vel, rvir, RRvir):
+        """Does the particle selection at the first time. If RRvir > 2 it does
+        selection mode 1, else does selection mode 2
+        """
+        if RRvir > 2:
+            self.ds.add_field(
+                ("stars", "r"),
+                function=lambda field, data: np.linalg.norm(data["stars", "coordinates"] - center, axis=1),
+                sampling_type="local",
+                units="Msun*km**2/s**2",
+                force_override=True
+            )
+            self.ds.add_field(
+                ("darkmatter", "r"),
+                function=lambda field, data: np.linalg.norm(data["darkmatter", "coordinates"] - center, axis=1),
+                sampling_type="local",
+                units="Msun*km**2/s**2",
+                force_override=True
+            )
+            self.compute_particle_kinetic(vel)
+            self.compute_particle_energy(v_cm)
+            
+            yt.add_particle_filter(
+                "stars_selection", 
+                function=lambda pfilter, data: (data[pfilter.filtered_type, "total_energy"] < 0) or (data[pfilter.filtered_type, "r"] < rvir), 
+                filtered_type="stars", 
+                requires=["total_energy"]
+            )
+            yt.add_particle_filter(
+                "darkmatter_selection", 
+                function=lambda pfilter, data: (data[pfilter.filtered_type, "total_energy"] < 0) or (data[pfilter.filtered_type, "r"] < rvir), 
+                filtered_type="darkmatter", 
+                requires=["total_energy"]
+            )
+            
+            sp = ds.sphere(center, 2 * rvir)
+            
+            
+
+        else:
+            raise Exception(f"You must provide RRvir>2")
+            
+        return particle_id_E 
+        
     def track_halos(self, halos_tbt, output, **kwargs):
         """Tracks the halos_tbt (to-be-tracked, not throw-back-thursday) halos. Each halos starting poit can be a different snapshot
         """
 
         if not isinstance(self.CompleteTree, pd.DataFrame) or not isinstance(self.equivalence_table, pd.DataFrame):
-            raise ValueError(f"Either CompleteTree or equivalence_table are not pandas dataframes! or are not properly set!")
+            raise ValueError("Either CompleteTree or equivalence_table are not pandas dataframes! or are not properly set!")
         
         self.halos_tbt = halos_tbt.sort_values("Snapshot")
         self._create_snapztdir()
         self._create_thtrees()
+
+        file_path = Path(output)
+        if file_path.is_file():
+            outpùt2 = output[:-5] + "_v2" + output[-5:]
+            warnings.warn(f"The file {output} already exists", RuntimeWarning)
+
         self._create_hdf5_file(fname=output)
 
-        active_halos = np.array([], dtype=int)
+        active_halos = set()
+        terminated_halos = set()
+        live_halos = set()
+        
         for index, row in self.snap_z_t_dir.iterrows():
             snapshot, redshift, time, pdir = row["Snapshot"], row["Redshift"], row["Time"], row["pdir"]
             
-            if snapshot in self.halos_tbt["Snapshot"].values: 
-                active_halos = np.array(
-                    active_halos, 
-                    self.halos_tbt[self.halos_tbt["Snapshot"] == snapshot]["Sub_tree_id"]
-                )
-            if active_halos.shape[0] != 0:
-                ds = config.loader
-            else:
+            inserted_halos = self.halos_tbt[self.halos_tbt["Snapshot"] == snapshot]["Sub_tree_id"].astype(int).values
+            active_halos.update(inserted_halos)
+                
+            live_halos = active_halos - terminated_halos
+            if not live_halos:
+                continue
+            if not active_halos:
                 raise Exception(f"You dont have any active halos at this point! (snapshot {snapshot}). This means that something went wrong!")
+            else:
+                self.ds = config.loader(pdir)
+                self.compute_particle_gravity()
 
-            for subtree in active_halos:
+
+            for subtree in live_halos:
+                if subtree in inserted_halos:
+                    member_particles = self.select_particles(
+                        center=self.ds.arr(self.halos_tbt[self.halos_tbt["Sub_tree_id"] == subtree][["position_x", "position_y", "position_z"]].astype(float).values, 'kpccm'),
+                        vel=self.ds.arr(self.halos_tbt[self.halos_tbt["Sub_tree_id"] == subtree][["velocity_x", "velocity_y", "velocity_z"]].astype(float).values, 'km/s'),
+                        rvir=self.ds.quan(self.halos_tbt[self.halos_tbt["Sub_tree_id"] == subtree]["virial_radius"].astype(float).values, 'kpccm'),
+                        RRvir=self.ds.quan(self.halos_tbt[self.halos_tbt["Sub_tree_id"] == subtree]["R/Rvir"].astype(float).values, '')
+                    )
+                else:
+                    pass
                 #pot + trackeo
                 #add particles to self.f
                 #compute statistics
                 #add statistics to csv
-            for subtree in active_halos:
+#            for subtree in live_halos:
                 #check for merging with main halo or merges between them using csvs. Now all of them are up to date.
-            
 
+            del self.ds
 
-
-
-
-
-
-
-                
         self.f.close()
-        return active_halos
+        return active_halos, live_halos, terminated_halos
                 
             
             
